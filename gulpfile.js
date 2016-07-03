b273f08008a0f05f3b1cd873106776970d4fcb19
Benedikt Rötsch <info@benedikt-roetsch.de>
2016-07-03 05:55:09 +0200

    fixes svgo configuration

--- gulpfile.js
+++ gulpfile.js
@@ -35,45 +35,48 @@ const svgSpriteConfig = {
     }
   },
   // This releases extra minimizing power of SVGO. Optional but recommended.
+
   shape: {
-    transform: {
-      svg: {
-        plugins: [
-          // This plugin converts all colors to currentColor.
-          // Read more about this here: https://css-tricks.com/cascading-svg-fill-color/
-          //
-          // Should not be used when you rely on the colors of your icons
-          {
-            convertColors: {
-              currentColor: true
-            }
-          },
-          // Optimize and minimize SVG shape
-          { collapseGroups: true },
-          { convertShapeToPath: true },
-          { mergePaths: true },
-          { transformsWithOnePath: true },
-          { convertPathData: true },
-          { convertTransform: true },
-          { convertStyleToAttrs: true },
-          // Remove noise from designer tools and clean up
-          { cleanupAttrs: true },
-          { removeDoctype: true },
-          { removeXMLProcInst: true },
-          { removeComments: true },
-          { removeMetadata: true },
-          { removeTitle: true },
-          { removeDesc: true },
-          { removeUselessStrokeAndFill: true },
-          { removeUnusedNS: true },
-          { removeRasterImages: true },
-          { removeUnknownsAndDefaults: true },
-          { removeNonInheritableGroupAttrs: true },
-          { cleanupListOfValues: true },
-          { cleanupNumericValues: true }
-        ]
+    transform: [
+      {
+        svgo: {
+          plugins: [
+            // This plugin converts all colors to currentColor.
+            // Read more about this here: https://css-tricks.com/cascading-svg-fill-color/
+            //
+            // Should not be used when you rely on the colors of your icons
+            {
+              convertColors: {
+                currentColor: true
+              }
+            },
+            // Optimize and minimize SVG shape
+            { collapseGroups: true },
+            { convertShapeToPath: true },
+            { mergePaths: true },
+            { transformsWithOnePath: true },
+            { convertPathData: true },
+            { convertTransform: true },
+            { convertStyleToAttrs: true },
+            // Remove noise from designer tools and clean up
+            { cleanupAttrs: true },
+            { removeDoctype: true },
+            { removeXMLProcInst: true },
+            { removeComments: true },
+            { removeMetadata: true },
+            { removeTitle: true },
+            { removeDesc: true },
+            { removeUselessStrokeAndFill: true },
+            { removeUnusedNS: true },
+            { removeRasterImages: true },
+            { removeUnknownsAndDefaults: true },
+            { removeNonInheritableGroupAttrs: true },
+            { cleanupListOfValues: true },
+            { cleanupNumericValues: true }
+          ]
+        }
       }
-    }
+    ]
   }
 }
 
